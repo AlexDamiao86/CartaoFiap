f4f90b251b8bcff397e13775dde3226baa8a341d
AlexDamiao86 <alex_maiabr@hotmail.com>
2022-07-10 17:01:27 -0300

    Adiciona migrations e versionamento db com flyway

--- src/main/java/br/com/fiap/cartao/entity/PlasticoCartao.java
+++ src/main/java/br/com/fiap/cartao/entity/PlasticoCartao.java
@@ -2,6 +2,7 @@ package br.com.fiap.cartao.entity;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,6 +13,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -31,6 +35,12 @@ public class PlasticoCartao {
 	@Enumerated(EnumType.STRING)
 	@Column(length = 30, precision = 6, scale = 2)
 	private SituacaoCartao situacao;
+	@Column(name = "data_cadastro", nullable = false, updatable = false)
+	@CreatedDate
+	private LocalDateTime dataCadastro;
+	@Column(name = "data_atualizacao", nullable = false, updatable = true)
+	@LastModifiedDate
+	private LocalDateTime dataUltimaAtualizacao;	
 	@ManyToOne
 	private Cliente cliente;
 
