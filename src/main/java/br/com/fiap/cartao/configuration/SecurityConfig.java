aaf31a37eb86f6c0661c21f050b5e88638d3d084
AlexDamiao86 <alex_maiabr@hotmail.com>
2022-08-02 12:12:05 -0300

    Permite que swagger-ui e h2-console sejam apresentados apos configurar Spring Security

--- src/main/java/br/com/fiap/cartao/configuration/SecurityConfig.java
+++ src/main/java/br/com/fiap/cartao/configuration/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -66,6 +67,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
 			.csrf().disable()
 			.formLogin().disable();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers(
+				"/**.html",
+				"/swagger-ui/**",
+				"/api-docs/**",
+				"/h2-console/**"
+				);
 	}
 	
 }
