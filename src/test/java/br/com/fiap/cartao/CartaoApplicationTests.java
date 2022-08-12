e588444bb9c84c58d64309e8d985024e7da0b7ac
AlexDamiao86 <alex_maiabr@hotmail.com>
2022-08-12 00:00:05 -0300

    Altera versao java para 1.8

---
+++ src/test/java/br/com/fiap/cartao/CartaoApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.fiap.cartao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CartaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+}
+
